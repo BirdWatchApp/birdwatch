9805fa2552bfbff986f969b4c7842df5a12f347b
Jacob Sasser <jacobsasser2010@gmail.com>
2023-10-17 14:00:53 -0600

    fixed all endpoints, tested with postman

--- client/index.js
+++ client/index.js
@@ -9,18 +9,18 @@
  * ************************************
  */
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App.jsx';
-import store from './store';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import App from './App.jsx';
+// import store from './store';
 
-const root = createRoot(document.getElementById('contents'));
-root.render(
-  // wrap the App in the Provider Component and pass in the store
+// const root = createRoot(document.getElementById('contents'));
+// root.render(
+//   // wrap the App in the Provider Component and pass in the store
 
-  <Provider store= {store}>
-    <App />
-  </Provider>,
+//   <Provider store= {store}>
+//     <App />
+//   </Provider>,
 
-);
+// );
