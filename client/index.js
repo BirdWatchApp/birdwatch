a223be4d8e0ce8e48d6feccbd513d7269bc8083b
leonathompson <tsonmart62@gmail.com>
2023-10-17 16:05:41 -0400

    changed index.js back to original redux boilerplate

--- client/index.js
+++ client/index.js
@@ -10,24 +10,24 @@
  */
 
 import React from 'react';
-import { ReactDOM } from 'react';
+// import { ReactDOM } from 'react';
 // import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import store from './store';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+// );
+const root = createRoot(document.getElementById('contents'));
+root.render(
+  // wrap the App in the Provider Component and pass in the store
+
   <Provider store= {store}>
     <App />
   </Provider>,
+
 );
-// const root = createRoot(document.getElementById('contents'));
-// root.render(
-//   // wrap the App in the Provider Component and pass in the store
-
-//   <Provider store= {store}>
-//     <App />
-//   </Provider>,
-
-// );
