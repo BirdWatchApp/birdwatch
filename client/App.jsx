0551aff92292e6e54d5df88fabb99e8587230452
Jacob Sasser <jacobsasser2010@gmail.com>
2023-10-18 10:15:10 -0600

    started working on login, working on fetch request, fixed page routing

--- client/App.jsx
+++ client/App.jsx
@@ -10,19 +10,27 @@
  */
 
 import React from 'react';
-import { render } from 'react-dom';
 import './styles/styles.css';
 import Login from './components/login.jsx';
 import SignUp from './components/signup.jsx';
 import BirdSearch from './components/birdSearch.jsx';
 import Posts from './components/posts.jsx';
 
+
 const App = () => {
+
+  const [pageState, setPageState] = React.useState('login');
+
   return(
         <div>
+          {pageState === 'login' && <Login setPageState={setPageState}/>}
+          {pageState === 'signup' && <SignUp setPageState={setPageState}/>}
+          {pageState === 'main' &&           
+          <div>
           <h1>BirdWatch</h1>
           <BirdSearch/>
           <Posts />
+          </div>}
         </div>
   );
 }
